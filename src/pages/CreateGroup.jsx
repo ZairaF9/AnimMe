465549e29af3aa78eef03887e0eb63e614b9d910
RedMedina <69237359+RedMedina@users.noreply.github.com>
2022-10-28 18:29:07 -0500

    Crear Grupos con Usuarios

--- src/pages/CreateGroup.jsx
+++ src/pages/CreateGroup.jsx
@@ -1,21 +1,33 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useNavigate} from 'react-router-dom';
 import {ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { auth,storage,db} from "../Firebase";
-import { AuthContext } from '../context/AuthContext';
-import { doc, setDoc} from "firebase/firestore";
+import {storage,db} from "../Firebase";
+import { doc, setDoc, onSnapshot, collection, query, where, getDocs} from "firebase/firestore";
+import {v4 as uuidv4} from 'uuid';
 
 const CreateGroup = () =>
 {
     const [err, setErr] = useState(false);
     const navigate = useNavigate();
-    const { currentUser } = useContext(AuthContext);
+
     const handleSubmit = async (e) =>
     {
         e.preventDefault();
         const NameGroup = e.target[0].value;
         const Carrera = e.target[1].value;
         const file = e.target[2].files[0];
+        let Gruopuuid = uuidv4();
+        var UsersGroupuid = [];
+        var UsersGroupName = [];
+
+        const q = query(collection(db, "users"), where("carrera", "==", Carrera));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            //console.log(doc.uid, " => ", doc.data().uid);
+            UsersGroupuid.push(doc.data().uid);
+            UsersGroupName.push(doc.data().displayName);
+        });
+
 
         try {
             const storageRef = ref(storage, NameGroup);
@@ -28,10 +40,12 @@ const CreateGroup = () =>
                  () =>
                  {
                     getDownloadURL(uploadTask.snapshot.ref).then( async (downloadURL) =>{
-                        await setDoc(doc(db,"Groups",currentUser.uid),{
-                            uid: currentUser.uid,
+                        await setDoc(doc(db,"Groups",Gruopuuid),{
+                            uid: Gruopuuid,
                             NameGroup,
                             Carrera,
+                            usersId: UsersGroupuid,
+                            usersName: UsersGroupName,
                             photoURL: downloadURL
                           });
                           navigate("/");
